a3aeb7775dcbdb2e0ad65c2efffa036689ae2e85
A. M Manzano Kharman <aidammk@hotmail.com>
2023-03-19 16:47:42 +0000

    added comments :)

--- tpop.py
+++ tpop.py
@@ -61,7 +61,7 @@ def checks_v2(child, named_cars, number_of_witnesses_needed, threshold):
     parent = child.parent
     parent_position = parent.claim_position()
 
-    #TODO: for each child of the parent, add the counter of the children and set that to the parent counter
+    
     if (
     #checking the parent is a neighbour of the child
     child.is_car_a_neighbour(parent) is True and
@@ -86,49 +86,75 @@ def checks_v2(child, named_cars, number_of_witnesses_needed, threshold):
     
 """ if len(witness_number_per_depth) != depth:
     raise Exception('Make sure there is number of witnesses defined for each round') """
-#call this function for car in cars
-def tpop(car, depth, witness_number_per_depth, threshold):
-    
-    for i in range(depth):
-        
-        number_of_witnesses_needed = witness_number_per_depth[i]
-        car_position = car.claim_position()
-        witnesses = car.name_witness(number_of_witnesses_needed)
-        
-        named_cars = set()
-        named_cars.add(car.ID)
 
-        if witnesses is None:
-            car.algorithm_honesty_output = False
-            
-        else:    
-            witnesses, named_cars = checks(car, car_position, witnesses, number_of_witnesses_needed, named_cars, threshold)
-
-            for witness in witnesses:
-                tpop(witness, depth -1, witness_number_per_depth, threshold)
 
 
 class Tree2:
 
     def __init__(self, prover, depth, n):
-
+        #prover is the root of the tree, and the agent calling this function
         self.prover = prover
+        #all nodes in the tree, indexed by depth level
         self.nodes = [[self.prover]]
         self.depth = depth
         
         for d in range(depth):
             
             s = []
+            #for all nodes in the given depth level
             for node in self.nodes[d]:
-                
+                #the node names some witnesses
                 witnesses = node.name_witness(n)
                 
                 for witness in witnesses:
+                    #we set the parent of that witness to be the node naming them
                     witness.parent = node
                     s.append(witness)
-                    
+                #and set the children of the nodes to be the named witnesses    
                 node.children = s
             self.nodes.append(s)
+
+
+
+def reverse_bfs(tree, witness_number_per_depth, threshold):
+
+    root = tree.prover
+    #we keep track of the agents named in each round
+    named_cars = set()
+    
+    #we start from the leaves and do a reverse BFS upwards until the root
+    for level in reversed(range(0, tree.depth + 1)):
+        #retrieve the number of witnesses(ie children) necessary at that level.
+        #NOTE: we start indexing from the 0th level.
+        number_of_witnesses_needed = witness_number_per_depth[level]
+
+        #for each depth level, we need to keep track of the number of children that approve the parent
+        parent_counter = 0
+        for child in tree.nodes[level]:
+            parent = child.parent
+            #if we are at the root, do not perform any more checks because the root has no parent
+            if child.parent is None:
+                break
+                
+            #return the number of approvals 
+            counter = checks_v2(child, named_cars, number_of_witnesses_needed, threshold)
+            #add the child to the set of visited/named agents
+            named_cars.add(child.ID)
+
+            #update the parent counter
+            parent_counter = parent_counter + counter
+            parent.counter = parent_counter
+
+    print('root counter ', root.counter, int(number_of_witnesses_needed * threshold))
+    #check that the root has enough approvals to be considered honest
+    if root.counter >= int(number_of_witnesses_needed * threshold):
+        root.algorithm_honesty_output = True
+    else:
+        root.algorithm_honesty_output = False
+            
+
+    return root.algorithm_honesty_output
+
 
 London = e.Environment([0,0.25], [0,0.25], 0.25)
 p = 1
@@ -148,44 +174,6 @@ witness_number_per_depth = [2, 2, 2]
 tree = Tree2(car_list[0], depth, 2)
 for d in range(depth + 1):
     print(tree.nodes[d])
-
-
-
-
-
-def reverse_bfs(tree, witness_number_per_depth, threshold):
-
-    root = tree.prover
-    named_cars = set()
-    
-
-    for level in reversed(range(0, tree.depth + 1)):
-        number_of_witnesses_needed = witness_number_per_depth[level]
-        #number_of_witnesses_needed = 2
-        
-        print(level)
-        parent_counter = 0
-        for child in tree.nodes[level]:
-            parent = child.parent
-            
-            
-            if child.parent is None:
-                break
-                
-            
-            counter = checks_v2(child, named_cars, number_of_witnesses_needed, threshold)
-            named_cars.add(child.ID)
-            parent_counter = parent_counter + counter
-            parent.counter = parent_counter
-
-    print('root counter ', root.counter, int(number_of_witnesses_needed * threshold))
-    if root.counter >= int(number_of_witnesses_needed * threshold):
-        root.algorithm_honesty_output = True
-    else:
-        root.algorithm_honesty_output = False
-            
-
-    return root.algorithm_honesty_output
 
 output = reverse_bfs(tree, witness_number_per_depth, 1)
 
